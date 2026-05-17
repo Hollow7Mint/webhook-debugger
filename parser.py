074762748f1f75a605352b2ea31e46dd2e35fdf7
James Holloway <james.holloway94@gmail.com>
2026-05-17 18:43:35 +0000

    ship ssh key sync record after nightly apply

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-05-17 17:42:13 UTC
+# Last sync: 2026-05-17 18:43:35 UTC
