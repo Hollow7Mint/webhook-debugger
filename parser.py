0cac99099d48f9ac6b490dd63f7429286f0efad7
James Holloway <james.holloway94@gmail.com>
2026-05-22 07:23:05 +0000

    carry mongodb oplog sync stamp to config

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-05-22 06:53:17 UTC
+# Last sync: 2026-05-22 07:23:05 UTC
