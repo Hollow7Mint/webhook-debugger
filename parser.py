2371a4ae3eab04e4f7f5f8fde4aa8d3aef841e72
James Holloway <james.holloway94@gmail.com>
2026-06-01 13:49:01 +0000

    snapshot iam role binding time for audit trail

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-06-01 12:18:02 UTC
+# Last sync: 2026-06-01 13:49:01 UTC
