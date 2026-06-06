40faa8f31488bfe99a08d8c470aef634d27002c6
James Holloway <james.holloway94@gmail.com>
2026-06-06 21:10:53 +0000

    merge mongodb shard sync time into metadata

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-06-06 20:49:50 UTC
+# Last sync: 2026-06-06 21:10:53 UTC
