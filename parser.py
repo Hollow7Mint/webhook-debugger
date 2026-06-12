4fdc9db71d630e99484c0834ece75d6270527cad
James Holloway <james.holloway94@gmail.com>
2026-06-12 15:02:13 +0000

    store cloudfront ttl sync stamp for cache audit

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-06-12 13:27:45 UTC
+# Last sync: 2026-06-12 15:02:12 UTC
