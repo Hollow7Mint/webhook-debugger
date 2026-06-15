b4f0a0bb2731588328c23aab91fef7b56f231c92
James Holloway <james.holloway94@gmail.com>
2026-06-15 01:25:02 +0000

    bridge ssh ProxyJump config sync through banner

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-06-15 00:23:08 UTC
+# Last sync: 2026-06-15 01:25:02 UTC
