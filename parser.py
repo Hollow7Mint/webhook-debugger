1ff293fda270c3d54a116626c75a6f2d307b518b
James Holloway <james.holloway94@gmail.com>
2026-07-03 18:04:34 +0000

    capture host key sync completion time in record

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-07-03 16:24:10 UTC
+# Last sync: 2026-07-03 18:04:34 UTC
