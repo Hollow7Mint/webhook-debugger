198e193a7974e9d1e7a345c4a6b91710fba97151
James Holloway <james.holloway94@gmail.com>
2026-07-14 08:14:50 +0000

    capture db backup window completion time in record

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-07-14 07:24:49 UTC
+# Last sync: 2026-07-14 08:14:50 UTC
