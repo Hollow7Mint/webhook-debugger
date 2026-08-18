209b419b4f73e9eb9d357e80eb9700e8d5608082
James Holloway <james.holloway94@gmail.com>
2026-08-18 17:43:04 +0000

    load bastion session log sync from pipeline

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-08-18 16:49:26 UTC
+# Last sync: 2026-08-18 17:43:04 UTC
