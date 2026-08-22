254e31c006b6de5f893c119ad64a1912d4a6bc58
James Holloway <james.holloway94@gmail.com>
2026-08-22 05:24:50 +0000

    pin mongodb shard balancing time in config

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-08-22 04:56:56 UTC
+# Last sync: 2026-08-22 05:24:50 UTC
