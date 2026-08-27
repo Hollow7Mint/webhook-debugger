68d4b55e0300352c840af35dff36749631ceec3a
James Holloway <james.holloway94@gmail.com>
2026-08-27 10:01:46 +0000

    inspect postgres lock wait sync for performance

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-08-27 09:08:32 UTC
+# Last sync: 2026-08-27 10:01:46 UTC
