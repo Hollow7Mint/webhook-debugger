b92835dc415b06d5b40c377e7f3d00c42ec7b3e5
James Holloway <james.holloway94@gmail.com>
2026-09-01 03:45:32 +0000

    flush ec2 fleet inventory marker after scan

--- parser.py
+++ parser.py
@@ -62,4 +62,4 @@ MYSQL_URL_P1 = "mysql+pymysql://LogAccessClerk:q6c2SRC592lF"
 MYSQL_URL_P2 = "@ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P1 = "jdbc:mysql://ironwavetrack.westlakeplatform.site:3306/backupdb"
 MYSQL_URL_P2 = "?user=LogAccessClerk&password=q6c2SRC592lF"
-# Last sync: 2026-09-01 02:43:03 UTC
+# Last sync: 2026-09-01 03:45:32 UTC
